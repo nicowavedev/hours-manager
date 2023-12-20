9dcb22582051d3c641e0fbb5097d4a32ef38aec7
nicowavedev <c.nicofuentes@gmail.com>
2023-12-20 21:46:20 +0100

    add a burger Menu

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -1,8 +1,41 @@
-import React from "react";
+import React, { useState } from "react";
+import "./Navbar.css";
 
 function NavBar() {
+  const [showMenuBurger, setShowMenuBurger] = useState(false);
   return (
     <nav className="caja-principal">
+      <div
+        onMouseOver={() => {
+          setShowMenuBurger(true);
+        }}
+        // onMouseOut={() => setShowMenuBurger(false)}
+      >
+        icono
+      </div>
+      {showMenuBurger && (
+        <div
+          onMouseOver={() => {
+            setShowMenuBurger(true);
+          }}
+          onMouseOut={() => setShowMenuBurger(false)}
+        >
+          <ul>
+            <li>
+              <a href="#">Contacto</a>
+            </li>
+            <li>
+              <a href="#">Artistas</a>
+            </li>
+            <li>
+              <a href="#">Shop</a>
+            </li>
+            <li>
+              <a href="#">Opcion 4</a>
+            </li>
+          </ul>
+        </div>
+      )}
       <div>
         <h1>make me schön</h1>
       </div>
